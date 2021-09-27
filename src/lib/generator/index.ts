a516439bd35d0fa23aaa6d812466431416ba9eb6
salah alhashmi <alhashmis246@gmail.com>
2021-09-27 09:15:31 -0700

    inlized

--- src/lib/generator/index.ts
+++ src/lib/generator/index.ts
@@ -6,8 +6,9 @@ export const objectToMap  = (config:any) => {
     const generatedMap = new Map();
 
     for(const option in config) {
-        console.log(config[option])
-        // for every loop set new [string, {all configs for that prop}]
+        // assert object
+        // set up object other function
+        // for every loop set new map data [string, {all configs for that prop}]
     }
     return generatedMap;
 }
---
+++ test/defaultValues.test.ts
@@ -0,0 +1,6 @@
+import {max, min} from '../src/lib/defaultValues';
+
+test("all values are set to default", () => {
+    expect(max).toBeNull();
+    expect(min).toBe(0);
+})
