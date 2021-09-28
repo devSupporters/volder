8abc325cab69aac89b5a170e17712bd26e769a9c
salah alhashmi <alhashmis246@gmail.com>
2021-09-27 23:46:00 -0700

    add assertObject function

--- src/lib/generator/index.ts
+++ src/lib/generator/index.ts
@@ -1,11 +1,12 @@
-// generally :
-// config the default values
+import {assertObject} from '../utils/assertObject';
 
 
 export const objectToMap  = (config:any) => {
     const generatedMap = new Map();
-
+    
     for(const option in config) {
+        assertObject(config[option]);
+
         // assert object
         // set up object other function
         // for every loop set new map data [string, {all configs for that prop}]
