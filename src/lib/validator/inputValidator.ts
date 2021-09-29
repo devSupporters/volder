4188aaafcec5a60939ad10b53c20863a6b6e5105
salah alhashmi <alhashmis246@gmail.com>
2021-09-29 01:18:11 -0700

    feat:inputValidator() function

--- src/lib/validator/inputValidator.ts
+++ src/lib/validator/inputValidator.ts
@@ -1,12 +1,64 @@
-export const inputValidator = (
-  input: any,
-  key:string,
-  validatorOption: object,
-  validInput:boolean,
-  errors: object
-) => {
-    // the operations 
-    // 1 => the type is true
-    // 2 => the min length true in (number | string)
-    // 3 => the max length true in (number | string)
+export const inputValidator = ( input: any, key:string, validatorOption: any, validInput:boolean, errors: any) => {
+    switch (validatorOption.type) {
+////////////////////////////////// String //////////////////////////////////////////
+        case String:
+            if(input[key].constructor.name !== "String") {
+                validInput = false;
+                errors[key] = `${key} should be a string`
+                break;
+            }
+
+            if(input[key].length < validatorOption.min) {
+                validInput = false;
+                errors[key] = `${key} should be at least ${validatorOption.min} length`
+                break;
+            }
+
+            if(input[key].length > validatorOption.max) {
+                validInput = false;
+                errors[key] = `${key} should be at most ${validatorOption.max} length`
+                break;
+            }
+            break;
+////////////////////////////////// Number /////////////////////////////////////////////
+        case Number:
+            if(input[key].constructor.name !== "Number") {
+                validInput = false;
+                errors[key] = `${key} should be a number`
+            }
+
+            if(input[key] < validatorOption.min) {
+                validInput = false;
+                errors[key] = `${key} should be at least ${validatorOption.min}`
+                break;
+            }
+
+            if(input[key] > validatorOption.max) {
+                validInput = false;
+                errors[key] = `${key} should be at most ${validatorOption.max}`
+                break;
+            }
+            break;
+
+        // case Boolean:
+        //     if(input[key].constructor.name !== "Boolean") {
+        //         validInput = false;
+        //         errors[key] = `${key} should be a boolean`
+        //     }
+        //     break;
+
+        // case Array:
+        //     if(input[key].constructor.name !== "Array") {
+        //         validInput = false;
+        //         errors[key] = `${key} should be a array`
+        //     }
+        //     break;
+
+        // case Object:
+        //     if(input[key].constructor.name !== "Object") {
+        //         validInput = false;
+        //         errors[key] = `${key} should be a object`
+        //     }
+        //     break;
+    }
 };
