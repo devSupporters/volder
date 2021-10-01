1a37ecbe66f39d362dd6f35db08525053bcedffa
salah alhashmi <alhashmis246@gmail.com>
2021-10-01 08:05:09 -0700

    feat:add boolean validator

---
+++ src/lib/validator/typeCasesValidators/boolean.ts
@@ -0,0 +1,12 @@
+export const booleanCase = (
+  input: any,
+  optionName: string,
+//   optionConfigs: any,
+  errors: any
+): void | boolean => {
+    const isBoolean = typeof input[optionName] === 'boolean' || input instanceof Boolean 
+    if (!isBoolean) {
+        errors[optionName] = `${optionName} should be a number`;
+        return false;
+      }
+};
--- src/lib/validator/validatorInput.ts
+++ src/lib/validator/validatorInput.ts
@@ -1,12 +1,15 @@
 import { stringCase } from './typeCasesValidators/string';
 import { numberCase } from './typeCasesValidators/number';
+import { booleanCase } from './typeCasesValidators/boolean';
+
 export const validatorInput = (input: any, optionName: string, optionConfigs: any, errors: any) => {
   switch (optionConfigs.type) {
     case String:
       return stringCase(input, optionName, optionConfigs, errors);
     case Number:
       return numberCase(input, optionName, optionConfigs, errors);
-    // case Boolean:
+    case Boolean:
+      return booleanCase(input, optionName, errors);
     // case Array:
     // case Object:
   }
