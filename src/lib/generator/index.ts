cb0f6226a4799e8f33766dde230de9276a5dd580
salah alhashmi <alhashmis246@gmail.com>
2021-10-02 07:56:04 -0700

    support just constructor function

--- src/lib/generator/index.ts
+++ src/lib/generator/index.ts
@@ -6,14 +6,23 @@ export const objectToMap = (config: any | object) => {
   const generatedMap = new Map();
 
   for (const option in config) {
-    assertObject(config[option]);
+    const types = [null, Boolean, Object, Number, String, Array];
+    if(types.includes(config[option])) {
+
+      config[option] = {type:config[option]}
+
+    } else {
+      assertObject(config[option], "Expected a (object | constructor function | null) but received a ");
+    }
 
     if (config[option].hasOwnProperty('type')) {
-      if (config[option].type !== null) {
+
+      if(config[option].type !== null ) {
         assertConstructorFunction(config[option].type);
       }
       const configuredOption = setUpOptionWithConfigs(config[option]);
       generatedMap.set(option, configuredOption);
+
     } else throw new Error(`type is required at ${option} property`);
   }
 
