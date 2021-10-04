51ec9224bf6eda7410b8e345cf9b736a2056ef64
salah alhashmi <alhashmis246@gmail.com>
2021-10-04 08:12:43 -0700

    fix

--- src/lib/generator/configSpliter.ts
+++ src/lib/generator/configSpliter.ts
@@ -14,18 +14,16 @@ export const configSpliter = (
     throw new TypeError(
       `Expected Array with two items [configuredValue, customError] but received empty Array at ${optionConfigName} property`
     );
-  };
-
-  if (optionProperty.length > 0) {
-    optionConfigType !== 'constructor-type'
-      ? assertType(optionProperty[0], optionConfigType, `${optionConfigName}[0] property`)
-      : assertConstructorFunction(optionProperty[0]);
-
-    defaultConfiguredOption[optionConfigName] = optionProperty[0];
-  };
-
-  if (optionProperty.length > 1) {
-    assertType(optionProperty[1], 'string', `${optionConfigName}[1] property`);
-    defaultConfiguredOption[optionConfigName + 'ErrorMessage'] = optionProperty[1];
+    
+  } else {
+      optionConfigType !== 'constructor-type'
+        ? assertType(optionProperty[0], optionConfigType, `${optionConfigName}[0] property`)
+        : assertConstructorFunction(optionProperty[0]);
+      defaultConfiguredOption[optionConfigName] = optionProperty[0];
+  
+    if (optionProperty.length > 1) {
+      assertType(optionProperty[1], 'string', `${optionConfigName}[1] property`);
+      defaultConfiguredOption[optionConfigName + 'ErrorMessage'] = optionProperty[1];
+    };
   };
 };
