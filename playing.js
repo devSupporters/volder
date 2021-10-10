4f4ef0e20d6061758b705af4b3304a30c01538a6
salah alhashmi <alhashmis246@gmail.com>
2021-10-10 10:22:12 -0700

    latest

--- playing.js
+++ playing.js
@@ -1,6 +1,3 @@
 // this file for testing by run:
 // > npm run build
 // > npm run dev
-// done
-const volder = require('./dist/index');
-console.log(volder);
--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,32 @@
-import babel from 'rollup-plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
+import { babel } from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import pkg from "./package.json";
 
+const banner = `
+/**
+ * @license
+ * author: ${pkg.author}
+ * ${pkg.moduleName}.js v${pkg.version}
+ * Released under the ${pkg.license} license.
+ */
+`;
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/index.js',
-    format: 'iife'
+    file: 'dist/index.cjs.js',
+    format: 'cjs',
+    exports: 'default',
+    banner
   },
   plugins: [
     babel({
-      exclude: 'node_modules/**'
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled'
     }),
-    resolve({
-      jsnext: true,
-    }),
+    nodeResolve(),
     commonjs(),
-    uglify()
-  ],
+    terser()
+  ]
 };
