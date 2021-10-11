2630a7f66b656d7a3b486b35fc91c35132670b40
salah alhashmi <alhashmis246@gmail.com>
2021-10-11 09:52:10 -0700

    add

--- playing.js
+++ playing.js
@@ -1,18 +1,16 @@
 // this file for testing by run:
 // > npm run build
 // > npm run dev
-const Volder = require('./dist/index.cjs');
+import Volder from 'volder';
 
-const volder1 = new Volder({
+const person = new Volder({
   name: {
-    type: String
+    type: String,
+    min: 4,
+    required: true
   },
-  age: Number
-});
-
-const volder2 = new Volder({
-  person: {
-    type: [volder1, 'must an object']
+  age: {
+    type: Number,
+    max: 100
   }
 });
-console.log(volder2.validate({ person: '' }));
