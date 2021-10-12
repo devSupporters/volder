7f959cb222e0abb7bccd1e44b873be6ccb0486f3
alguerocode <75932477+alguerocode@users.noreply.github.com>
2021-10-12 05:36:39 -0700

    Update playing.js

--- playing.js
+++ playing.js
@@ -2,16 +2,16 @@
 // > npm run start # to run rollup --watch server - auto building -
 // > npm run dev   # to run node server
 
-const Volder = require('./dist/index.cjs');
+// const Volder = require('./dist/index.cjs');
 
-const person = new Volder({
-  name: {
-    type: String,
-    min: 4,
-    required: true
-  },
-  age: {
-    type: Number,
-    max: 100
-  }
-});
+// const person = new Volder({
+//   name: {
+//     type: String,
+//     min: 4,
+//     required: true
+//   },
+//   age: {
+//     type: Number,
+//     max: 100
+//   }
+// });
