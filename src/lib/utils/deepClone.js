49fa88e7b1065f053a29d8ca792a071bcb434553
salah alhashmi <alhashmis246@gmail.com>
2021-10-30 18:36:47 +0400

    add clone method

--- src/lib/utils/deepClone.js
+++ src/lib/utils/deepClone.js
@@ -1,50 +1,42 @@
 export function deepClone(item) {
-    if (!item) { return item; } 
+  if (!item) {
+    return item;
+  }
 
-    let types = [ Number, String, Boolean ], 
-        result;
+  let types = [Number, String, Boolean];
+  let result;
 
-    // normalizing primitives if someone did new String('aaa'), or new Number('444');
-    types.forEach((type) => {
-        if (item instanceof type) {
-            result = type( item );
-        }
-    });
-
-    if (typeof result == "undefined") {
-        if (Object.prototype.toString.call( item ) === "[object Array]") {
-            result = [];
-            item.forEach(function(child, index, array) { 
-                result[index] = clone( child );
-            });
-        } else if (typeof item == "object") {
-            // testing that this is DOM
-            if (item.nodeType && typeof item.cloneNode == "function") {
-                result = item.cloneNode( true );    
-            } else if (!item.prototype) { // check that this is a literal
-                if (item instanceof Date) {
-                    result = new Date(item);
-                } else {
-                    // it is an object literal
-                    result = {};
-                    for (let i in item) {
-                        result[i] = clone( item[i] );
-                    }
-                }
-            } else {
-                // depending what you would like here,
-                // just keep the reference, or create new object
-                if (false && item.constructor) {
-                    // would not advice to do that, reason? Read below
-                    result = new item.constructor();
-                } else {
-                    result = item;
-                }
-            }
-        } else {
-            result = item;
-        }
+  // normalizing primitives if someone did new String('aaa'), or new Number('444');
+  types.forEach((type) => {
+    if (item instanceof type) {
+      result = type(item);
     }
+  });
 
-    return result;
+  if (typeof result == 'undefined') {
+    if (Object.prototype.toString.call(item) === '[object Array]') {
+      result = [];
+      item.forEach((child, index) => {
+        result[index] = deepClone(child);
+      });
+    } else if (typeof item == 'object') {
+      if (!item.prototype) {
+        // check that this is a literal
+        //   if (item instanceof Date) {
+        //     result = new Date(item);
+        //   } else {
+        // it is an object literal
+        result = {};
+        for (let i in item) {
+          result[i] = deepClone(item[i]);
+        }
+      } else {
+        result = item;
+      }
+    } else {
+      result = item;
+    }
+  }
+
+  return result;
 }
--- src/lib/validator/index.js
+++ src/lib/validator/index.js
@@ -1,39 +1,42 @@
 import { validateInput } from './validate-input';
 import { Volder } from '../volder';
+import { deepClone } from '../utils/deepClone';
 
 export const validator = (volderMap, input, collectErrors = true) => {
+  const clonedInput = deepClone(input);
   const errors = {};
   let valid = true;
-  
+
   volderMap.forEach((optionConfigs, optionName) => {
     let validCurInput = true;
 
     // check if option is required
-    if (optionConfigs.required === true && !input.hasOwnProperty(optionName)) {
+    if (optionConfigs.required === true && !clonedInput.hasOwnProperty(optionName)) {
       if (collectErrors) errors[optionName] = optionConfigs.requiredErrorMessage || `${optionName} is required`;
       validCurInput = false;
-    } else if (input.hasOwnProperty(optionName)) {
+    } else if (clonedInput.hasOwnProperty(optionName)) {
       if (optionConfigs.type instanceof Volder) {
-        const isObject = typeof input[optionName] === 'object' && !Array.isArray(input[optionName]) && input[optionName] !== null;
+        const isObject =
+          typeof clonedInput[optionName] === 'object' &&
+          !Array.isArray(clonedInput[optionName]) &&
+          clonedInput[optionName] !== null;
 
         if (isObject) {
-          const validationResult = validator(optionConfigs.type.volderMap, input[optionName], collectErrors);
-          
+          const validationResult = validator(optionConfigs.type.volderMap, clonedInput[optionName], collectErrors);
+
           validCurInput = collectErrors ? validationResult.valid : validationResult;
-          
+
           if (collectErrors && Object.keys(validationResult.errors).length > 0) errors[optionName] = validationResult.errors;
-          
         } else {
           if (collectErrors) errors[optionName] = optionConfigs.typeErrorMessage || `${optionName} should be an object`;
           validCurInput = false;
         }
-      } else validCurInput = validateInput(input, optionName, optionConfigs, errors, collectErrors);
-
+      } else validCurInput = validateInput(clonedInput, optionName, optionConfigs, errors, collectErrors);
     }
     // validCurInput sometimes equal undefined, so we need to strict equal to false;
     if (!validCurInput) valid = false;
   });
 
-  if (collectErrors) return { valid, errors };
+  if (collectErrors) return { valid, errors, value: clonedInput };
   else return valid;
 };
