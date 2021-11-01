d61966e8483be25d0148d34dda43e73b6ab66db0
salah alhashmi <alhashmis246@gmail.com>
2021-11-01 16:55:35 +0400

    test:add more type validation test

--- test/type-validation.test.js
+++ test/type-validation.test.js
@@ -198,4 +198,264 @@ test('Number type validation', () => {
   });
 });
 
-test('Object type validation', () => {});
+test('Array type validation', () => {
+  const ArrSchema = new Volder({
+    arrType: Array,
+    arrRequired: { type: Array, required: true },
+    arrMin: { type: Array, min: 3 },
+    arrMax: { type: Array, max: 5 }
+  });
+
+  const obj1 = { arrType: [1, 3], arrRequired: ['exists'], arrMax: [1, 2, 3, 4, 5], arrMin: [1, 2, 3] };
+  const obj2 = { arrType: true, arrRequired: ['exists'] };
+  const obj3 = {};
+  const obj4 = { arrMax: [1, 2, 3, 4, 5, 6], arrRequired: ['exists'] };
+  const obj5 = { arrMin: [1, 2], arrRequired: ['exists'] };
+
+  expect(ArrSchema.validate(obj1)).toEqual({
+    valid: true,
+    errors: {},
+    value: obj1
+  });
+  expect(ArrSchema.validate(obj2)).toEqual({
+    valid: false,
+    errors: {
+      arrType: 'arrType should be an array'
+    },
+    value: {}
+  });
+  expect(ArrSchema.validate(obj3)).toEqual({
+    valid: false,
+    errors: {
+      arrRequired: 'arrRequired is required'
+    },
+    value: {}
+  });
+  expect(ArrSchema.validate(obj4)).toEqual({
+    valid: false,
+    errors: {
+      arrMax: 'arrMax should be at most 5 items'
+    },
+    value: {}
+  });
+  expect(ArrSchema.validate(obj5)).toEqual({
+    valid: false,
+    errors: {
+      arrMin: 'arrMin should be at least 3 items'
+    },
+    value: {}
+  });
+
+  const ArrSchemaErrorMessage = new Volder({
+    arrType: { type: [Array, 'just array type'] },
+    arrRequired: { type: Array, required: [true, 'should be exists'] },
+    arrMin: { type: Array, min: [3, 'the min length is 3'] },
+    arrMax: { type: Array, max: [5, 'the max length is 5'] }
+  });
+
+  expect(ArrSchemaErrorMessage.validate(obj1)).toEqual({
+    valid: true,
+    errors: {},
+    value: obj1
+  });
+  expect(ArrSchemaErrorMessage.validate(obj2)).toEqual({
+    valid: false,
+    errors: {
+      arrType: 'just array type'
+    },
+    value: {}
+  });
+  expect(ArrSchemaErrorMessage.validate(obj3)).toEqual({
+    valid: false,
+    errors: {
+      arrRequired: 'should be exists'
+    },
+    value: {}
+  });
+  expect(ArrSchemaErrorMessage.validate(obj4)).toEqual({
+    valid: false,
+    errors: {
+      arrMax: 'the max length is 5'
+    },
+    value: {}
+  });
+  expect(ArrSchemaErrorMessage.validate(obj5)).toEqual({
+    valid: false,
+    errors: {
+      arrMin: 'the min length is 3'
+    },
+    value: {}
+  });
+});
+
+test('Object type validation', () => {
+  const ObjSchema = new Volder({
+    objType: Object,
+    objRequired: { type: Object, required: true }
+  });
+
+  const obj1 = { objType: { name: 'max' }, objRequired: { here: true } };
+  const obj2 = { objType: 'string', objRequired: { here: true } };
+  const obj3 = {};
+
+  expect(ObjSchema.validate(obj1)).toEqual({
+    valid: true,
+    errors: {},
+    value: obj1
+  });
+  expect(ObjSchema.validate(obj2)).toEqual({
+    valid: false,
+    errors: {
+      objType: 'objType should be an object'
+    },
+    value: {}
+  });
+  expect(ObjSchema.validate(obj3)).toEqual({
+    valid: false,
+    errors: {
+      objRequired: 'objRequired is required'
+    },
+    value: obj3
+  });
+
+  const ObjSchemaErrorMessage = new Volder({
+    objType: { type: [Object, 'the valid is object'] },
+    objRequired: { type: Object, required: [true, 'should be exists'] }
+  });
+
+  expect(ObjSchemaErrorMessage.validate(obj1)).toEqual({
+    valid: true,
+    errors: {},
+    value: obj1
+  });
+  expect(ObjSchemaErrorMessage.validate(obj2)).toEqual({
+    valid: false,
+    errors: {
+      objType: 'the valid is object'
+    },
+    value: {}
+  });
+  expect(ObjSchemaErrorMessage.validate(obj3)).toEqual({
+    valid: false,
+    errors: {
+      objRequired: 'should be exists'
+    },
+    value: obj3
+  });
+});
+
+test('boolean type validation', () => {
+  const BoolSchema = new Volder({
+    boolType: Boolean,
+    boolRequired: { type: Boolean, required: true }
+  });
+
+  const obj1 = { boolType: false, boolRequired: true };
+  const obj2 = { boolType: [1, 3, 3], boolRequired: true };
+  const obj3 = {};
+
+  expect(BoolSchema.validate(obj1)).toEqual({
+    valid: true,
+    errors: {},
+    value: obj1
+  });
+  expect(BoolSchema.validate(obj2)).toEqual({
+    valid: false,
+    errors: {
+      boolType: 'boolType should be a boolean (true or false)'
+    },
+    value: {}
+  });
+  expect(BoolSchema.validate(obj3)).toEqual({
+    valid: false,
+    errors: {
+      boolRequired: 'boolRequired is required'
+    },
+    value: obj3
+  });
+
+  const BoolSchemaErrorMessage = new Volder({
+    boolType: { type: [Boolean, 'must true or false'] },
+    boolRequired: { type: Boolean, required: [true, 'boolean is empty'] }
+  });
+
+  expect(BoolSchemaErrorMessage.validate(obj1)).toEqual({
+    valid: true,
+    errors: {},
+    value: obj1
+  });
+  expect(BoolSchemaErrorMessage.validate(obj2)).toEqual({
+    valid: false,
+    errors: {
+      boolType: 'must true or false'
+    },
+    value: {}
+  });
+  expect(BoolSchemaErrorMessage.validate(obj3)).toEqual({
+    valid: false,
+    errors: {
+      boolRequired: 'boolean is empty'
+    },
+    value: obj3
+  });
+});
+
+test('null type validation', () => {
+  const NullSchema = new Volder({
+    nullType: null,
+    nullRequired: { type: null, required: true },
+    nullAvoid: { type: null, avoid: [null, String, Boolean] }
+  });
+
+  const obj1 = { nullType: 'str', nullRequired: 'is required', nullAvoid: 23 };
+  const obj2 = {};
+  const obj3 = { nullAvoid: true, nullRequired: false };
+
+  expect(NullSchema.validate(obj1)).toEqual({
+    valid: true,
+    errors: {},
+    value: obj1
+  });
+  expect(NullSchema.validate(obj2)).toEqual({
+    valid: false,
+    errors: {
+      nullRequired: 'nullRequired is required'
+    },
+    value: {}
+  });
+  expect(NullSchema.validate(obj3)).toEqual({
+    valid: false,
+    errors: {
+      nullAvoid: 'Boolean type not allowed'
+    },
+    value: {}
+  });
+
+  const NullSchemaErrorMessage = new Volder({
+    nullType: null,
+    nullRequired: { type: null, required: [true, "null required must exists"] },
+    nullAvoid: { type: [null, "null string boolean type not valid"], avoid: [null, String, Boolean] }
+  });
+
+  expect(NullSchemaErrorMessage.validate(obj1)).toEqual({
+    valid: true,
+    errors: {},
+    value: obj1
+  });
+  expect(NullSchemaErrorMessage.validate(obj2)).toEqual({
+    valid: false,
+    errors: {
+      nullRequired:"null required must exists"
+    },
+    value: {}
+  });
+
+  obj3.nullAvoid = null;
+  expect(NullSchemaErrorMessage.validate(obj3)).toEqual({
+    valid: false,
+    errors: {
+      nullAvoid: "null string boolean type not valid"
+    },
+    value: {}
+  });
+});
