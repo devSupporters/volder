ea9d0b6244cabd46fc76d639d839491fe175773e
salah alhashmi <alhashmis246@gmail.com>
2021-12-14 20:29:52 +0400

    feature: upper and lower case for string

--- src/lib/validator/configs-validators/alphanumeric.js
+++ src/lib/validator/configs-validators/alphanumeric.js
@@ -6,9 +6,9 @@ export const validateAlphanumeric = (input) => {
     if (
       !(code > 47 && code < 58) && // numeric (0-9)
       !(code > 64 && code < 91) && // upper alpha (A-Z)
-      !(code > 96 && code < 123)
+      !(code > 96 && code < 123)  // lower alpha (a-z)
     ) {
-      // lower alpha (a-z)
+     
       return false;
     }
   }
