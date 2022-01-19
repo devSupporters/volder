7ad1bdfdb616baa172f6e0aa32e681e962d6b0cb
salah alhashmi <alhashmis246@gmail.com>
2022-01-19 13:46:16 +0400

    fire: delete some unneeded lines

--- src/lib/generator/configs/string/trim.js
+++ src/lib/generator/configs/string/trim.js
@@ -1,7 +1,7 @@
 import { assertType } from "../../../utils/assert-type";
 
 export const setupTrimConfig = (optionConfigs) => {
-  if (optionConfigs.hasOwnProperty('trim') && optionConfigs.type === String) {
+  if (optionConfigs.hasOwnProperty('trim')) {
     assertType(optionConfigs.trim, 'boolean', 'trim property');
   }
 };
