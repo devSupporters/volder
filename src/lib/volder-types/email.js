f690f8a5a04224b5923ed23b133ff4b4c4f5cfdc
batu <bipci@myseneca.ca>
2022-02-02 12:31:53 -0500

    regexp escape character correction:
    the escape character in email body was unnecessary and was causing fail in the test

--- src/lib/volder-types/email.js
+++ src/lib/volder-types/email.js
@@ -1,5 +1,5 @@
 export const Email = (input) => {
-  const regex = /^[\w.+~&\-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,6}$/
-
+  const regex = /^[\w.+~&]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,6}$/
+  //I have removed the unnecessary escape from the [email body]
   return regex.test(String(input))
 };
