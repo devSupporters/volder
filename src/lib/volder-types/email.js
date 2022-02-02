c82e39b0cf466808dc4a192dc22a697154a4acc4
salah alhashmi <75932477+alguerocode@users.noreply.github.com>
2022-02-02 22:40:22 +0400

    fix: email validation, first index contain letter , accept some symbols

--- src/lib/volder-types/email.js
+++ src/lib/volder-types/email.js
@@ -1,4 +1,9 @@
 export const Email = (input) => {
-  const regex = /^[\w.+~&]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,6}$/
+  const regex = /^[\w.~!#$%&'*+-/=?^_`{|]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,6}$/;
+  const code = String(input).charCodeAt(0);
+
+  if (!(code > 64 && code < 91) && !(code > 96 && code < 123)) {
+    return false;
+  }
   return regex.test(String(input))
 };
