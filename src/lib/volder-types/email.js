77b98e40d824ed3c2bf6fabc22b67439b1e75059
salah alhashmi <75932477+alguerocode@users.noreply.github.com>
2022-02-02 21:48:53 +0400

    remove comment

--- src/lib/volder-types/email.js
+++ src/lib/volder-types/email.js
@@ -1,5 +1,4 @@
 export const Email = (input) => {
   const regex = /^[\w.+~&]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,6}$/
-  //I have removed the unnecessary escape from the [email body]
   return regex.test(String(input))
 };
