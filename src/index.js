6af8e55901cdffdc1dee8f8c6fabce0a6dd3db0e
salah alhashmi <75932477+alguerocode@users.noreply.github.com>
2022-03-02 06:40:18 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import { Json } from './lib/volder-types/json';
 import { CreditCard } from './lib/volder-types/credit-card';
 import { IPAddress } from './lib/volder-types/ip-address';
 import { URL } from './lib/volder-types/url';
-export { Volder, singleVolder, UUID, Email,Json, CreditCard, URL, IPAddress };
+export { Volder, singleVolder, UUID, Email, Json, CreditCard, URL, IPAddress };
